171b5b0dce2a157e3e4069a07bc0211552f94019
Andrea Minetti wavein.ch <andrea.minetti@wavein.ch>
2016-05-17 19:29:39 +0200

    Completed feedbacks

--- app/controllers/MailerController.scala
+++ app/controllers/MailerController.scala
@@ -5,10 +5,12 @@ import javax.inject.{Inject,Named}
 import akka.actor.ActorRef
 import models.Formatters._
 import models._
-import play.api.libs.json.Json
+import play.api.Configuration
+import play.api.libs.json.{JsValue, Json}
+import play.api.libs.ws.WSClient
 import play.api.mvc._
 import sender.{Messages, CampaignSupervisor}
-import services.{AuthService, MailService, Template}
+import services.{Hash, AuthService, MailService, Template}
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
@@ -18,7 +20,9 @@ import scala.concurrent.Future
   */
 class MailerController @Inject()(
                                   authService: AuthService,
+                                  conf: Configuration,
                                   mailer: MailService,
+                                  wsClient: WSClient,
                                   @Named(CampaignSupervisor.name) campaignSupervisor: ActorRef
                                 ) extends Controller {
 
@@ -79,20 +83,55 @@ class MailerController @Inject()(
     }
   }
 
-  def complaint() = Action{ r =>
-    println("Complaint:" + r.body)
-    Ok("received")
+
+
+  implicit val feedbackFormatter = Json.format[Feedback]
+  case class Feedback(
+                              reason:String,
+                              mailId:String,
+                              fromMail:String,
+                              hashedMail:Seq[String],
+                              timestamp:String
+                            )
+  def bounce() = Action.async{ r =>
+    val url = conf.getString("adt.frontend").get + "/newsletter/bounces"
+    val msg = message(r.body.asText.get)
+    val feedback = message2Feedback(msg,"bounce")
+    println("bounce:" + msg)
+    sendFeedback(feedback,url).map{_ => Ok("received") }
   }
 
-  def bounce() = Action{ r =>
-    println("Bounce:" + r.body)
-    Ok("received")
+  def complaint() = Action.async{ r =>
+    val url = conf.getString("adt.frontend").get + "/newsletter/bounces"
+    val msg = message(r.body.asText.get)
+    val feedback = message2Feedback(msg,"complaint")
+    println("complaint:" + msg)
+    sendFeedback(feedback,url).map{_ => Ok("received") }
   }
 
-  def success() = Action{ r =>
-    println("Success:" + (Json.parse(r.body.asText.get) \ "Message"))
-    Ok("received")
+  def success() = Action.async{ r =>
+    val url = conf.getString("adt.frontend").get + "/newsletter/delivery"
+    val msg = message(r.body.asText.get)
+    val feedback = message2Feedback(msg,"success")
+    println("success:" + msg)
+    sendFeedback(feedback,url).map{_ => Ok("received") }
   }
 
+
+
+
+  private def message(body:String):JsValue = Json.parse((Json.parse(body) \ "Message").as[String])
+
+  private def message2Feedback(msg:JsValue,reason:String) = Feedback(
+    reason = reason + (msg \ "bounce" \ "bounceType").asOpt[String].map(b => "-"+b).getOrElse(""),
+    mailId = (msg \ "mail" \ "messageId").as[String],
+    fromMail = (msg \ "mail" \ "source").as[String],
+    hashedMail = (msg \ "mail" \ "destination").as[Seq[String]].map(Hash.hashEmail),
+    timestamp = (msg \ "mail" \ "timestamp").as[String]
+  )
+
+  private def sendFeedback(feedback:Feedback,url:String):Future[Boolean] = {
+     wsClient.url(url).post(Json.toJson(feedback)).map(_.status == 200)
+  }
 
 }
